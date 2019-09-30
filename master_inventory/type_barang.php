bd4cd117885e344de667aab5e491b07a5fb6fdfc
LafterUI <bintangpamungkas206@gmail.com>
2019-09-30 08:54:41 +0700

    type barang (sama benerin kayak master satuan)

--- master_inventory/type_barang.php
+++ master_inventory/type_barang.php
@@ -1,4 +1,68 @@
-<?php $title = "Master Satuan"; ?>
+<!-- TOLONG BENERIN GAN -->
+<!-- 
+==================== YANG EROR ========================
+    -- GAK ADA
+    -- CUMA BENERIN EDIT NYA (PAS DI KLIK EDIT FORM TAMBAHNYA DI GANTI EDIT, GW GAK BISA BIKIN KEK GITU)
+
+====================== HOSHI ==========================
+ -->
+
+<!-- konek -->
+<?php 
+  $host = 'localhost';
+  $user = 'root';
+  $password = '';
+  $dbname = 'brasco_pusat';
+  $conn = mysqli_connect($host, $user, $password, $dbname);
+?>
+
+<!-- query -->
+<?php
+  //tambah 
+  if(isset($_POST['tambah'])){
+    $nama = $_POST['nama'];
+    $sql = "INSERT INTO tipe_barang (nama_barang) VALUE ('$nama')";
+    $query = mysqli_query($conn, $sql);
+    if ($query) {
+      echo "<script>alert('Data Added')</script>";
+      // echo "<script>location='type_barang.php'</script>";
+    }
+    else{
+      echo "<script>alert('F')</script>";
+    }
+  }
+
+  // edit
+  if (isset($_POST['edit'])) {
+    $nama = $_POST['nama'];
+    // $id= $_POST['id'];
+    $edit = $conn->query("UPDATE tipe_barang SET nama_barang='$nama' WHERE id='$_GET[id]'");
+    if ($edit) {
+      echo "<script>alert('Data Added')</script>";
+      echo "<script>location='type_barang.php'</script>"; 
+    }
+    else{
+      echo "<script>alert('F')</script>"; 
+    }
+  }
+
+// delete
+  if (isset($_GET['stats'])) {
+    if ($_GET['stats'] == 'delete') {
+      $delete = $conn->query("DELETE FROM tipe_barang WHERE id='$_GET[id]'");
+      if ($delete) {
+        // echo "<script>alert('Data deleted')</script>";
+        echo "<script>location='type_barang.php'</script>";
+      }
+      else{
+        echo "<script>alert('FFF')</script>";
+      }
+    }
+  }
+
+?>
+
+<?php $title = "Type Barang"; ?>
 <script>
     var active = 'header_inventory';
     var active_2 = 'header_type_barang';
@@ -45,33 +109,59 @@
           				<div class="form-group">
           					<label class="col-sm-3 control-label">Type Barang</label>
           					<div class="col-sm-7">
-          						<input type="text" name="" class="form-control" required="">
+          						<input type="text" name="nama" class="form-control" required="">
           					</div>
-                    <button type="submit" class="btn btn-info">Add</button>
+                    <button type="submit" class="btn btn-info" name="tambah">Add</button>
           				</div>
           				
           			</div>
           		</div>
           	</form>
+
+            <?php if (isset($_GET['stats'])) : ?>  
+              <?php if ($_GET['stats'] == 'edit') : ?>
+                <?php $k = $conn->query('SELECT * FROM tipe_barang WHERE id="'.$_GET['id'].'"') ?>
+                <?php $ok = $k->fetch_assoc(); ?>
+                <form action="" method="POST" class="form-horizontal">
+                  <div class="box-body">
+                    <div class="col-sm-8 pad">
+                      <div class="form-group">
+                        <label class="col-sm-3 control-label">Type Barang</label>
+                        <div class="col-sm-7">
+                          <input type="text" class="form-control" required="" name="nama" value="<?php echo $ok['nama_barang'] ?>">
+                        </div>
+                        <button type="text" class="btn btn-info" name="edit">Save</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              <?php endif; ?>
+            <?php endif; ?>
           </div>
 
           <div class="data-table">
           	<table id="example1" class="table table-bordered table-striped text-center">
           		<thead>
           			<tr>
+                  <th>No</th>
           				<th>Type Barang</th>
           				<th>Aksi</th>
           			</tr>
           		</thead>
-          		<tbody>
-          			<tr>
-          				<td>Nama Satuan disini...</td>
-          				<td>
-          					<a href="#"><i class="fa fa-edit fa-lg text-primary" style="padding-left: 20px;"></i></a>
-          					<a href="#"><i class="fa fa-trash-o fa-lg text-red" style="padding-left: 20px;"></i></a>
-          				</td>
-          			</tr>
-          		</tbody>
+          		<?php $nomor =1; ?>
+              <?php 
+              $tampil = $conn->query("SELECT * FROM tipe_barang");
+              while ($p = $tampil->fetch_assoc()) : ?>
+                <tr>
+                  <td><?php echo $nomor; ?></td>
+                  <td><?php echo $p['nama_barang'] ?></td>
+                  <td>
+                    <a href="master_inventory/type_barang.php?stats=edit&id=<?= $p['id'] ?>"><i class="fa fa-edit fa-lg text-primary" style="padding-left: 20px;"></i></a>
+                    <a href="master_inventory/type_barang.php?stats=delete&id=<?=$p['id']?>"  onclick="return confirm('Data akan dihapus?')"><i class="fa fa-trash-o fa-lg text-red" style="padding-left: 20px;"></i></a>
+                  </td>
+                </tr>
+                <?php $nomor++; ?>
+              <?php endwhile; ?>
           	</table>
 
 
