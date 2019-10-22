6020d1b08d1a2a49f8cb304b948ba73f7751b743
hythes <hanifsetyananda17@gmail.com>
2019-10-22 18:06:28 +0700

    update db

--- purchase_order/index.php
+++ purchase_order/index.php
@@ -168,7 +168,7 @@ $title = 'Purchase Order';
                             <div class="form-group">
                                 <label class="col-sm-4 control-label">DPP</label>
                                 <div class="col-sm-8">
-                                    <input type="number" name="dpp" id="dpp" class="form-control">
+                                    <input type="number" name="dpp" readonly id="dpp" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group" style="margin-top: 50px;">
@@ -226,6 +226,7 @@ $title = 'Purchase Order';
         'satuan': '',
         'total': 0,
         'sata': '',
+        'dpp': 0
     };
     var active = 'header_po';
     var active_2 = 'header_purchase_order';
@@ -273,8 +274,8 @@ $title = 'Purchase Order';
             request: 'cari_satuan',
             data: dataSimpan.satuan
         }, function(data) {
+
             data = JSON.parse(data);
-            console.log(data.satuan);
             dataSimpan.sata = data.satuan;
 
             var barcode = $('#barcode_po').val();
@@ -294,7 +295,6 @@ $title = 'Purchase Order';
                 'harga': harga,
                 'satuan': dataSimpan.satuan
             })
-
             $('#table_po').append(
                 '<tr id="tr_po_' + dataSimpan.i + '">' +
                 '<td>' + dataSimpan.i + '</td>' +
@@ -311,7 +311,9 @@ $title = 'Purchase Order';
             cek_tipe_ppn();
             dataSimpan.i++;
             dataSimpan.total += (parseInt(quantity) * parseInt(harga));
+            dataSimpan.dpp = dataSimpan.total;
             $('#total').val(dataSimpan.total);
+            $('#dpp').val(dataSimpan.dpp);
             $('#data_po').val(JSON.stringify(simpanArray));
         })
     })
@@ -326,11 +328,11 @@ $title = 'Purchase Order';
         if ($('#tipe_ppn_i').is(':checked')) {
             dpp = dpp * (10 / 11);
             ppn = dpp * (10 / 100);
-            $('#ppn').val(parseFloat(ppn).toFixed(2));
+            $('#ppn').val('(10/11) * 10%');
         }
         if ($('#tipe_ppn_e').is(':checked')) {
             ppn = dpp * (10 / 100);
-            $('#ppn').val(parseFloat(ppn).toFixed(2));
+            $('#ppn').val("10%");
         }
         return ppn;
     }
