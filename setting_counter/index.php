ae1d7d69449fbeb5e3df5443ae842dd58c6b3e14
LafterUI <bintangpamungkas206@gmail.com>
2019-11-05 14:21:33 +0700

    done setting counter

--- setting_counter/index.php
+++ setting_counter/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = "Setting Counter";
 
-if (isset($_POST['simpan'])) {
+if (isset($_POST['header'])) {
     $sql = '';
     extract($_POST);
     for ($i = 0; $i < count($_POST['header']); $i++) {
@@ -31,31 +31,31 @@ if (isset($_POST['simpan'])) {
                     <h4><i class="icon fa fa-info"></i> Informasi!</h4>
                     Format no counter ini bisa di setting bulanan atau bulanan, jadi andai bulanan setiap bulan di edit untuk reset no counter kembali
                 </div>
-                <form action="" method="POST">
+                <form action="" method="POST" id="form">
                     <div class="data-table">
                         <div class="table-responsive">
-                            <table id="example1" class="table table-bordered table-striped ">
+                            <table id="eee" class="table table-bordered table-striped ">
                                 <thead>
                                     <th>Nama Counter</th>
                                     <th>Header 2(digit)</th>
                                     <th>No Counter 8(digit)</th>
                                 </thead>
                                 <tbody>
-                                    <?php foreach (query("SELECT * FROM counter") as $data) : ?>
+                                    <?php $i = 0;  foreach (query("SELECT * FROM counter") as $data) : ?>
                                         <tr>
-                                            <td><?= $data['nama'] ?></td>
+                                            <td id="nama<?=$i?>"><?= $data['nama'] ?></td>
                                             <td><input type="text" name="header[]" value="<?= $data['header'] ?>" class="form-control"> </td>
-                                            <td><input type="number" name="digit[]" value="<?= $data['digit'] ?>" class="form-control"></td>
+                                            <td ><input type="number" id="digit<?=$i?>"  name="digit[]" value="<?= $data['digit'] ?>" class="validasi form-control"></td>
                                             <input type="hidden" name="tabel[]" value="<?= $data['tabel'] ?>">
                                         </tr>
-                                    <?php endforeach; ?>
+                                    <?php $i++; endforeach; ?>
                                 </tbody>
                             </table>
                         </div>
                     </div>
                     <div class="pull-right pad">
                         <button class="btn btn-danger">Reset</button>
-                        <button class="btn btn-info" name="simpan" type="submit">Simpan</button>
+                        <button class="btn btn-info" name="simpan" type="submit" >Simpan</button>
                     </div>
                 </form>
 
@@ -66,4 +66,38 @@ if (isset($_POST['simpan'])) {
 <script>
     var active = 'header_counter'
 </script>
+
+<script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
+<script>
+    $('.validasi').keyup(function(){
+        if($(this).val() > 100000000){
+            alert("maksimal inputan harus 8");
+        }
+        return false;
+    });
+    var switch2 = false;
+    $('#form').submit((e)=>{
+        e.preventDefault();
+        var i = $('.validasi').val()
+        $('#eee > tbody > tr ').each(e =>{
+            switch2 = false;
+            var b = $('#digit'+e).val();
+            console.log(b.length)
+            if(b.length < 8 || b.length > 8){
+                alert($('#nama'+e).html() +' harus 8 Digit!');
+                switch2 = true;
+                return false;
+            }
+            
+        })
+        $('#eee').promise().done(()=>{
+            if(switch2 === false){
+            $('#form').submit()
+            }
+        })
+    })
+    
+
+</script>
+
 <?php include('../templates/footer.php') ?>
